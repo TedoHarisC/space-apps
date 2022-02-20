174bb605595d3dd20840f90eb8d35ef374623e9a
TedoHarisC <tedoharischandra29@gmail.com>
2022-02-20 13:30:42 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Image.asset(
+            'assets/logo_dark.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBlackColor = Color(0xff222222);
+Color kBlackAccentColor = Color(0xff2A2B37);
+Color kGreyColor = Color(0xff94959B);
+Color kLineDarkColor = Color(0xffEAEAEA);
+Color kWhiteGreyColor = Color(0xffF1F1F5);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kRedColor = Color(0xffFF314A);
+Color kYellowColor = Color(0xffFFBA00);
+Color kBlueColor = Color(0xff2972FF);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+TextStyle blackTextStyle = GoogleFonts.nunito(
+  color: kBlackColor,
+);
+
+TextStyle blackAccentTextStyle = GoogleFonts.nunito(
+  color: kBlackAccentColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.nunito(
+  color: kGreyColor,
+);
+
+TextStyle lineDarkTextStyle = GoogleFonts.nunito(
+  color: kLineDarkColor,
+);
+
+TextStyle whiteGreyTextStyle = GoogleFonts.nunito(
+  color: kWhiteGreyColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.nunito(
+  color: kWhiteColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.nunito(
+  color: kRedColor,
+);
+
+TextStyle yellowTextStyle = GoogleFonts.nunito(
+  color: kYellowColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.nunito(
+  color: kBlueColor,
+);
