b9a82856316688c239c7ee26788a429c8bd135ee
TedoHarisC <tedoharischandra29@gmail.com>
2022-04-23 08:15:37 +0700

    Menambahkan Halaman Detail Page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:space/theme.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Color indicatorColor = Color(0xff394A54);
+  double indicatorMargin = 5;
+  int currentIndex = 1;
+  bool isExpand = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +24,7 @@ class DetailPage extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(top: 95),
             child: Image.asset(
-              'assets/image_detail1.png',
+              'assets/image_detail$currentIndex.png',
             ),
           ),
           GestureDetector(
@@ -51,68 +61,232 @@ class DetailPage extends StatelessWidget {
                     ),
                     color: kWhiteColor,
                   ),
+                  child: NotificationListener(
+                    onNotification: (Notification notif) {
+                      if (notif is ScrollEndNotification) {
+                        if (notif.metrics.minScrollExtent == -1.0) {
+                          setState(() {
+                            isExpand = true;
+                          });
+                        } else {
+                          setState(() {
+                            isExpand = false;
+                          });
+                        }
+                      }
+
+                      return true;
+                    },
+                    child: SingleChildScrollView(
+                      controller: scrollController,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 16),
+                            Center(
+                              child: Container(
+                                width: 30,
+                                height: 4,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: kLineDarkColor,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Poang Children',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                    fontWeight: semiBold,
+                                  ),
+                                ),
+                                Text(
+                                  '\$219',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'IKOE',
+                              style: blackTextStyle.copyWith(
+                                fontSize: 18,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Container(
+                              height: 50,
+                              child: Stack(
+                                alignment: Alignment.centerLeft,
+                                children: [
+                                  Row(
+                                    children: [
+                                      colorIndicator(Color(0xff394A54), 0),
+                                      colorIndicator(Color(0xffEBAB23), 1),
+                                      colorIndicator(Color(0xff757477), 2),
+                                      colorIndicator(Color(0xff29282C), 3),
+                                      colorIndicator(Color(0xffECE9DA), 4),
+                                    ],
+                                  ),
+                                  AnimatedContainer(
+                                    duration: Duration(milliseconds: 300),
+                                    width: 30,
+                                    height: 30,
+                                    margin: EdgeInsets.only(
+                                      left: indicatorMargin,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: indicatorColor,
+                                      border: Border.all(
+                                        width: 3,
+                                        color: kWhiteColor,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Bringing new life to an old favourite. We first introduced this chair in the 1950’s. Some 60 years later we brought it back into the range with the same craftsmanship, comfort and appearance. Enjoy!',
+                              style: greyTextStyle.copyWith(
+                                fontWeight: semiBold,
+                                height: 1.8,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Bringing new life to an old favourite. We first introduced this chair in the 1950’s. Some 60 years later we brought it back into the range with the same craftsmanship, comfort and appearance. Enjoy!',
+                              style: greyTextStyle.copyWith(
+                                fontWeight: semiBold,
+                                height: 1.8,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Bringing new life to an old favourite. We first introduced this chair in the 1950’s. Some 60 years later we brought it back into the range with the same craftsmanship, comfort and appearance. Enjoy!',
+                              style: greyTextStyle.copyWith(
+                                fontWeight: semiBold,
+                                height: 1.8,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Bringing new life to an old favourite. We first introduced this chair in the 1950’s. Some 60 years later we brought it back into the range with the same craftsmanship, comfort and appearance. Enjoy!',
+                              style: greyTextStyle.copyWith(
+                                fontWeight: semiBold,
+                                height: 1.8,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Bringing new life to an old favourite. We first introduced this chair in the 1950’s. Some 60 years later we brought it back into the range with the same craftsmanship, comfort and appearance. Enjoy!',
+                              style: greyTextStyle.copyWith(
+                                fontWeight: semiBold,
+                                height: 1.8,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 );
               },
             ),
           ),
         ],
       ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(20),
-            topLeft: Radius.circular(20),
-          ),
-          color: kWhiteColor,
-          boxShadow: [
-            BoxShadow(
-              color: kGreyColor,
-              blurRadius: 5,
-              offset: Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            Container(
-              width: 56,
-              height: 56,
-              margin: EdgeInsets.only(right: 16),
+      bottomNavigationBar: isExpand
+          ? null
+          : Container(
+              padding: EdgeInsets.all(20),
               decoration: BoxDecoration(
-                color: kWhiteGreyColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Image.asset(
-                  'assets/icon_cart.png',
-                  width: 24,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  topLeft: Radius.circular(20),
                 ),
+                color: kWhiteColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: kGreyColor,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
               ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                height: 56,
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: kBlackColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(14),
+              child: Row(
+                children: [
+                  Container(
+                    width: 56,
+                    height: 56,
+                    margin: EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: kWhiteGreyColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/icon_cart.png',
+                        width: 24,
+                      ),
                     ),
                   ),
-                  child: Text(
-                    'Buy Now',
-                    style: whiteTextStyle.copyWith(
-                      fontSize: 16,
-                      fontWeight: semiBold,
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      height: 56,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          backgroundColor: kBlackColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                        ),
+                        child: Text(
+                          'Buy Now',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
-          ],
+    );
+  }
+
+  Widget colorIndicator(Color color, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          indicatorColor = color;
+          indicatorMargin = 5 + (index * 60);
+          currentIndex = index + 1;
+        });
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
         ),
       ),
     );
